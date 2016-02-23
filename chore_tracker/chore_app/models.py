7a3540e0013909976a2956a558b0779f88c033b9
Ye Vang <yvang@macalester.edu>
2016-02-23 15:33:26 -0600

    Added bootstrap modules, and updated the Models script to contain housemate/chore entities

--- chore_tracker/chore_app/models.py
+++ chore_tracker/chore_app/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
+import datetime
+from django.db import models
+from django.utils import timezone
+from django.forms import fields
+
 # Create your models here.
+class Housemate(models.Model):
+	'''Housemate independent entity.'''
+	person = models.Manager()
+	first_name = models.CharField('First name', max_length = 40)
+	last_name = models.CharField('Last name', max_length = 40)
+
+	def __str__(self):
+		'''Used for administrators to turn a housemate object to a string
+		by first and last name'''
+		return '%s %s' % (self.first_name, self.last_name)
+
+	full_name = property(__str__)
+
+
+class Chore(models.Model):
+	'''Chore dependent entity that is linked to a Housemate entity.'''
+	assigned_to = models.ForeignKey(Housemate, on_delete = models.CASCADE)
+	chore_title = models.CharField(max_length = 200)
+	create_date = timezone.now()
+	due_date = models.DateTimeField(default = '')
+	is_done = models.BooleanField('Status', default = False)
+
+	def __str__(self):
+		return self.chore_title
