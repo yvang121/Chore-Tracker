3d016997c9277a3e19c987bd0c16e404b2affd00
Ye Vang <yvang@macalester.edu>
2016-02-26 14:06:16 -0600

    Updated the main urls script

--- chore_tracker/chore_app/urls.py
+++ chore_tracker/chore_app/urls.py
@@ -3,3 +3,9 @@ from django.conf.urls import url
 from . import views
 
 app_name = 'chore_app'
+
+urlpatterns = [
+	url(r'^$', views.IndexView.as_view(), name='index'),
+	url(r'^(?P<housemate_id>[0-9]+)/$', views.detail, name='detail'),
+	url(r'^(?P<housemate_id>[0-9]+)/addChore$', views.addChore, name='addChore'),
+]
