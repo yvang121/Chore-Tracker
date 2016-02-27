111c6bae50ff36f12cdf7e5c1cfc7ce39968913f
Ye Vang <yvang@macalester.edu>
2016-02-26 22:24:11 -0600

    Added new 'Add Housemate' URL

--- chore_tracker/chore_app/urls.py
+++ chore_tracker/chore_app/urls.py
@@ -7,5 +7,6 @@ app_name = 'chore_app'
 urlpatterns = [
 	url(r'^$', views.IndexView.as_view(), name='index'),
 	url(r'^(?P<housemate_id>[0-9]+)/$', views.detail, name='detail'),
-	url(r'^(?P<housemate_id>[0-9]+)/addChore$', views.addChore, name='addChore'),
+	url(r'^(?P<housemate_id>[0-9]+)/add_chore$', views.addChore, name='addChore'),
+	url(r'^new_housemate$', views.addHousemate, name='addHousemate'),
 ]
