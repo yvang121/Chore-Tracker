cf3e4782cf87533b5c575a4a4d2e52aa196d8589
Ye Vang <yvang@macalester.edu>
2016-03-04 11:55:02 -0600

    Updated the forms classes to have default input fields

--- chore_tracker/chore_app/forms.py
+++ chore_tracker/chore_app/forms.py
@@ -1,17 +1,19 @@
 from django import forms
-
+import datetime
 from .models import Chore
 from .models import Housemate
 
-class ChoreForm(forms.ModelForm):
+class ChoreForm(forms.Form):
 	class Meta:
 		model = Chore
 		fields = [
 		"chore_title",
 		"due_date",
 		]
+	chore_title = forms.CharField(max_length = 200)
+	due_date = forms.DateTimeField(initial = datetime.datetime.now())
 
-class HousemateForm(forms.ModelForm):
+class HousemateForm(forms.Form):
 	class Meta:
 		model = Housemate
 		fields = [
@@ -19,3 +21,6 @@ class HousemateForm(forms.ModelForm):
 		"last_name",
 		"email",
 		]
+	first_name = forms.CharField(max_length = 100)
+	last_name = forms.CharField(max_length = 100)
+	email = forms.EmailField(max_length = 300)
