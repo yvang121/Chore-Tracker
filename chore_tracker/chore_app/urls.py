a5c921f40354890675a8a7ada1e578ed1d53f698
Ye Vang <yvang@macalester.edu>
2016-03-21 16:40:15 -0500

    Changed <chore_title> to <chore_id> in url patterns

--- chore_tracker/chore_app/urls.py
+++ chore_tracker/chore_app/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
 	url(r'^(?P<housemate_id>[0-9]+)/add_chore$', views.addChore, name='addChore'),
 	url(r'^new_housemate$', views.addHousemate, name='addHousemate'),
 	url(r'^(?P<housemate_id>[0-9]+)/delete$', views.delete, name='delete'),
-	url(r'^(?P<housemate_id>[0-9]+)/(?P<chore_title>[a-zA-Z0-9_ ]+)/delete$', views.delete, name='delete'),
+	url(r'^(?P<housemate_id>[0-9]+)/(?P<chore_id>[a-zA-Z0-9_ ]+)/delete$', views.delete, name='delete'),
 ]
