d242702403a29f4387b71509faa11a6e2d746465
Ye Vang <yvang@macalester.edu>
2016-03-21 17:34:49 -0500

    Added edit info url

--- chore_tracker/chore_app/urls.py
+++ chore_tracker/chore_app/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
 	url(r'^(?P<housemate_id>[0-9]+)/$', views.detail, name='detail'),
 	url(r'^(?P<housemate_id>[0-9]+)/add_chore$', views.addChore, name='addChore'),
 	url(r'^new_housemate$', views.addHousemate, name='addHousemate'),
-	url(r'^(?P<housemate_id>[0-9]+)/delete$', views.delete, name='delete'),
 	url(r'^(?P<housemate_id>[0-9]+)/(?P<chore_id>[a-zA-Z0-9_ ]+)/delete$', views.delete, name='delete'),
+	url(r'^(?P<housemate_id>[0-9]+)/editInfo$', views.editInfo, name='editInfo'),
 ]
