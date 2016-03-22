70c5636cee4ce8dd6e898589574000beee7914c2
hsonsalla <hsonsall@macalester.edu>
2016-03-22 15:16:19 -0500

    Changed formatting on date time widget.

--- chore_tracker/chore_app/forms.py
+++ chore_tracker/chore_app/forms.py
@@ -6,6 +6,7 @@ from .models import Chore
 from .models import Housemate
 
 class ChoreForm(forms.Form):
+
 	class Meta:
 		model = Chore
 		fields = [
@@ -13,9 +14,14 @@ class ChoreForm(forms.Form):
 		"due_date",
 		]
 
+	dateTimeOptions = {
+	'format': 'mm/dd/yyyy HH:ii P',
+	'showMeridian' : True
+	}
+
 	chore_title = forms.CharField(max_length = 200)
-	due_date = forms.DateTimeField(widget=DateTimeWidget(bootstrap_version=3), initial = datetime.datetime.now, 
-		input_formats = ['%d/%m/%Y %H:%M'])
+	due_date = forms.DateTimeField(widget=DateTimeWidget(bootstrap_version=3, options=dateTimeOptions), initial = datetime.datetime.now, 
+		input_formats = ['%m/%d/%Y %I:%M %p'])
 
 
 class HousemateForm(forms.Form):
