dd53a267f582fb4df51e319cef3910d509d7b737
Ye Vang <yvang@macalester.edu>
2016-03-21 23:21:17 -0500

    Added worded tags to urls for readability

--- chore_tracker/chore_app/urls.py
+++ chore_tracker/chore_app/urls.py
@@ -6,9 +6,9 @@ app_name = 'chore_app'
 
 urlpatterns = [
 	url(r'^$', views.IndexView.as_view(), name='index'),
-	url(r'^(?P<housemate_id>[0-9]+)/$', views.detail, name='detail'),
+	url(r'^housemate/(?P<housemate_id>[0-9]+)/$', views.detail, name='detail'),
 	url(r'^(?P<housemate_id>[0-9]+)/add_chore$', views.addChore, name='addChore'),
 	url(r'^new_housemate$', views.addHousemate, name='addHousemate'),
-	url(r'^(?P<housemate_id>[0-9]+)/(?P<chore_id>[a-zA-Z0-9_ ]+)/delete$', views.delete, name='delete'),
+	url(r'^(?P<housemate_id>[0-9]+)/chores/(?P<chore_id>[a-zA-Z0-9_ ]+)/delete$', views.delete, name='delete'),
 	url(r'^(?P<housemate_id>[0-9]+)/editInfo$', views.editInfo, name='editInfo'),
 ]
