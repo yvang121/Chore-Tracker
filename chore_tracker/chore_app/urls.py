32827003a79977b86935c3cc46a748333ad0648e
Ye Vang <yvang@macalester.edu>
2016-03-23 02:37:13 -0500

    Refactored urls to have word 'housemate' in it

--- chore_tracker/chore_app/urls.py
+++ chore_tracker/chore_app/urls.py
@@ -7,8 +7,9 @@ app_name = 'chore_app'
 urlpatterns = [
 	url(r'^$', views.IndexView.as_view(), name='index'),
 	url(r'^housemate/(?P<housemate_id>[0-9]+)/$', views.detail, name='detail'),
-	url(r'^(?P<housemate_id>[0-9]+)/add_chore$', views.addChore, name='addChore'),
+	url(r'^housemate/(?P<housemate_id>[0-9]+)/add_chore$', views.addChore, name='addChore'),
 	url(r'^new_housemate$', views.addHousemate, name='addHousemate'),
-	url(r'^(?P<housemate_id>[0-9]+)/chores/(?P<chore_id>[a-zA-Z0-9_ ]+)/delete$', views.delete, name='delete'),
-	url(r'^(?P<housemate_id>[0-9]+)/editInfo$', views.editInfo, name='editInfo'),
+	url(r'^housemate/(?P<housemate_id>[0-9]+)/chores/(?P<chore_id>[a-zA-Z0-9_ ]+)/delete_chore$', views.deleteChore, name='deleteChore'),
+	url(r'^housemate/(?P<housemate_id>[0-9]+)/delete_housemate$', views.deleteHousemate, name='deleteHousemate'),
+	url(r'^housemate/(?P<housemate_id>[0-9]+)/editInfo$', views.editInfo, name='editInfo'),
 ]
