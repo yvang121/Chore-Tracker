cbb7f10a2a47ef567418f4594e3eb898544d19ae
Ye Vang <yvang@macalester.edu>
2016-04-04 13:21:50 -0500

    Updated methods to take house id as input parameter and passing that to the html

--- chore_tracker/chore_app/views.py
+++ chore_tracker/chore_app/views.py
@@ -3,14 +3,12 @@ from django.http import HttpResponseRedirect, HttpResponse, Http404
 from django.core.urlresolvers import reverse
 from django.views import generic
 from django.utils import timezone 
-
-import datetime 
 from datetime import timedelta
-
+import datetime
 from django.contrib.auth.views import password_reset_confirm
-
 from .forms import ChoreForm, HousemateForm, LogInForm, SignUpForm
 from .models import Housemate, Chore
+from house.models import House
 
 # Regsitration
 def home(request):
@@ -33,29 +31,23 @@ def home(request):
 		}
 	return render(request, "chore_app/home.html", context)
 
-class IndexView(generic.ListView):
-	'''Displays a generic display for a particular object. The input
-	needs to know the model attribute in order to do so. Ideally, this
-	function will display a list of housemate names, wherein a user can click
-	on the housemate name link that will lead to a list of chores for that
-	housemate.
-	'''
-	template_name = 'chore_app/index.html'
-	model = Housemate
-	context_object_name = 'alphabetical_order'
-	
-	def get_queryset(self):
-		'''Return the most recent by last name'''
-		return Housemate.person.order_by('last_name')
+def index(request, house_id):
+	'''Renders the html page that views the housemates for a given house'''
+	house = get_object_or_404(House, pk=house_id)
+	housemates = house.housemate_set.all()
+	context = {'housmates': housemates, 'house': house,
+	}
+	return render(request, 'chore_app/index.html', context)
 
-def detail(request, housemate_id):
+def detail(request, house_id, housemate_id):
 	'''Renders an HTML page to view the details for a housemate given a
 	housemate ID'''
+	house = get_object_or_404(House, pk=house_id)
 	housemate = get_object_or_404(Housemate, pk = housemate_id)
 	today = datetime.datetime.date(datetime.datetime.now()) 
-	tomorrow = today + timedelta(1)
-	past = today - timedelta(1)
-	upcoming = tomorrow + timedelta(1)
+	tomorrow = today + datetime.timedelta(1)
+	past = today - datetime.timedelta(1)
+	upcoming = tomorrow + datetime.timedelta(1)
 	current_time = datetime.datetime.time(datetime.datetime.now())
 	mintime = datetime.time.min
 	maxtime = datetime.time.max
@@ -66,11 +58,12 @@ def detail(request, housemate_id):
 		datetime.datetime.combine(tomorrow, maxtime)))
 	upcoming_set = housemate.chore_set.filter(due_date__gte=(datetime.datetime.combine(tomorrow, maxtime)))
 	context = {'housemate': housemate, 'today_set': today_set, 'tomorrow_set': tomorrow_set, 
-	'overdue_set': overdue_set, 'upcoming_set': upcoming_set}
+	'overdue_set': overdue_set, 'upcoming_set': upcoming_set, 'house': house,}
 	return render(request, 'chore_app/detail.html', context)
 
-def addChore(request, housemate_id):
+def addChore(request, house_id, housemate_id):
 	'''Creates a chore data instance for a given housemate id'''
+	house = get_object_or_404(House, pk=house_id)
 	housemate = get_object_or_404(Housemate, pk = housemate_id)
 	form = ChoreForm(request.POST or None)
 	if request.method == 'POST':
@@ -80,15 +73,17 @@ def addChore(request, housemate_id):
 			chore_title = form.cleaned_data['chore_title']
 			due_date = form.cleaned_data['due_date']
 			post = Chore.chore_manager.create(chore_title=chore_title, due_date=due_date, assigned_to=assigned_to)
-			return HttpResponseRedirect(reverse('chore_app:detail', args=[housemate_id]))
+			return HttpResponseRedirect(reverse('chore_app:detail', args=[house_id, housemate_id]))
 	context = {
     'form': form,
     'housemate': housemate,
+    'house': house,
     }
 	return render(request, 'chore_app/addChore.html', context)
 
-def addHousemate(request):
+def addHousemate(request, house_id):
 	'''Creates a housemate instance'''
+	house = get_object_or_404(House, pk=house_id)
 	form = HousemateForm(request.POST or None)
 	if request.method == 'POST':
 		form = HousemateForm(request.POST)
@@ -96,19 +91,22 @@ def addHousemate(request):
 			first_name = form.cleaned_data['first_name'].capitalize()
 			last_name = form.cleaned_data['last_name'].capitalize()
 			email = form.cleaned_data['email']
-			post = Housemate.person.create(first_name=first_name, last_name=last_name, email=email)
-			return HttpResponseRedirect('/chore_app/index/')
-	context = {
-    "form": form,
+			post = Housemate.person.create(house=house, first_name=first_name, last_name=last_name, email=email)
+			return HttpResponseRedirect(reverse('chore_app:index', args=[house_id]))
+	context = {"form": form, 'house': house
     }
 	return render(request, 'chore_app/addHousemate.html', context)
 
-def deleteChore(request, housemate_id, chore_id):
+def deleteChore(request, house_id, housemate_id, chore_id):
+	'''Takes a chore_id to delete chore, that belongs to a housemate'''
+	house = get_object_or_404(House, pk=house_id)
 	housemate = get_object_or_404(Housemate, pk=housemate_id)
 	instance = housemate.chore_set.filter(pk=chore_id).delete()
-	return HttpResponseRedirect(reverse('chore_app:detail', args=[housemate_id]))
+	return HttpResponseRedirect(reverse('chore_app:detail', args=[house_id, housemate_id]))
 
-def editInfo(request, housemate_id):
+def editInfo(request, house_id, housemate_id):
+	'''Takes an Http request, and a housemate id to edit housemate info'''
+	house = get_object_or_404(House, pk=house_id)
 	housemate = get_object_or_404(Housemate, pk=housemate_id)
 	if request.method == 'POST':
 		if 'first_name' in request.POST and request.POST['first_name'] != '':
@@ -118,9 +116,19 @@ def editInfo(request, housemate_id):
 		if 'email' in request.POST and request.POST['email'] != '':
 			housemate.email = request.POST['email']
 		housemate.save()
-		return HttpResponseRedirect('/chore_app/index')
-	return render(request, 'chore_app/editInfo.html')
+		return HttpResponseRedirect(reverse('chore_app:index', args=[house_id]))
+	context = {
+	'housemate': housemate,
+	'house': house,
+	}
+	return render(request, 'chore_app/editInfo.html', context)
 
-def deleteHousemate(request, housemate_id):
+def deleteHousemate(request, house_id, housemate_id):
+	'''Deletes a housemate, given housemate id'''
+	house = get_object_or_404(House, pk=house_id)
 	housemate = get_object_or_404(Housemate, pk=housemate_id).delete()
-	return HttpResponseRedirect('/chore_app/index')
+	context = {
+	'house': house,
+	'housemate': housemate,
+	}
+	return HttpResponseRedirect(reverse('chore_app:index', args=[house_id]))
