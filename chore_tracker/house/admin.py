fb2a99cf3d471d701315dd05e3f67e7bb660f485
Ye Vang <yvang@macalester.edu>
2016-04-04 13:12:01 -0500

    Added housemate field to admin page

--- chore_tracker/house/admin.py
+++ chore_tracker/house/admin.py
@@ -2,5 +2,17 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import House
+from chore_app.models import Housemate
 
-admin.site.register(House)
+class HousemateInline(admin.TabularInline):
+    model = Housemate
+    extra = 2
+
+class HouseAdmin(admin.ModelAdmin):
+    fieldsets = [
+    (None,      {'fields':['house_name', 'zip_code']}),
+    ]
+    inlines = [HousemateInline]
+    search_fields = ['house_name', 'zip_code', 'housemate.full_name']
+
+admin.site.register(House, HouseAdmin)
