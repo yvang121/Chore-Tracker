0cf2ffe56892dc61c191b153a5c59733cf92e1d2
Ye Vang <yvang@macalester.edu>
2016-04-05 23:40:40 -0500

    Removed 'home' from url

--- chore_tracker/chore_app/urls.py
+++ chore_tracker/chore_app/urls.py
@@ -7,10 +7,10 @@ app_name = 'chore_app'
 
 urlpatterns = [
 	url(r'^$',views.home,name='home'),
-	url('house/(?P<house_id>[0-9]+)/home/$', views.index, name='index'),
+	url(r'^house/(?P<house_id>[0-9]+)/$', views.index, name='index'),
+	url(r'^house/(?P<house_id>[0-9]+)/add_housemate$', views.addHousemate, name='addHousemate'),
 	url(r'^house/(?P<house_id>[0-9]+)/housemate/(?P<housemate_id>[0-9]+)/chore-summary$', views.detail, name='detail'),
 	url(r'^house/(?P<house_id>[0-9]+)/housemate/(?P<housemate_id>[0-9]+)/add_chore$', views.addChore, name='addChore'),
-	url(r'^house/(?P<house_id>[0-9]+)/home/add_housemate$', views.addHousemate, name='addHousemate'),
 	url(r'^house/(?P<house_id>[0-9]+)/housemate/(?P<housemate_id>[0-9]+)/chores/(?P<chore_id>[a-zA-Z0-9_ ]+)/delete_chore$', views.deleteChore, name='deleteChore'),
 	url(r'^house/(?P<house_id>[0-9]+)/housemate/(?P<housemate_id>[0-9]+)/delete_housemate$', views.deleteHousemate, name='deleteHousemate'),
 	url(r'^house/(?P<house_id>[0-9]+)/housemate/(?P<housemate_id>[0-9]+)/editInfo$', views.editInfo, name='editInfo'),
