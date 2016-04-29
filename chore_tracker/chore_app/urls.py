ab62aca8f2f95c68859c137d2927dfb9713e7198
Chen <violet_zhao@126.com>
2016-04-29 13:58:13 -0500

    Changed editHousemate, choreSummary names

--- chore_tracker/chore_app/urls.py
+++ chore_tracker/chore_app/urls.py
@@ -9,9 +9,9 @@ urlpatterns = [
 	url(r'^$',views.home,name='home'),
 	url(r'^house/(?P<house_id>[0-9]+)/$', views.index, name='index'),
 	url(r'^house/(?P<house_id>[0-9]+)/add_housemate$', views.addHousemate, name='addHousemate'),
-	url(r'^house/(?P<house_id>[0-9]+)/housemate/(?P<housemate_id>[0-9]+)/summary$', views.detail, name='detail'),
+	url(r'^house/(?P<house_id>[0-9]+)/housemate/(?P<housemate_id>[0-9]+)/summary$', views.choreSummary, name='choreSummary'),
 	url(r'^house/(?P<house_id>[0-9]+)/housemate/(?P<housemate_id>[0-9]+)/add_chore$', views.addChore, name='addChore'),
 	url(r'^house/(?P<house_id>[0-9]+)/housemate/(?P<housemate_id>[0-9]+)/chores/(?P<chore_id>[a-zA-Z0-9_ ]+)/delete_chore$', views.deleteChore, name='deleteChore'),
 	url(r'^house/(?P<house_id>[0-9]+)/housemate/(?P<housemate_id>[0-9]+)/delete_housemate$', views.deleteHousemate, name='deleteHousemate'),
-	url(r'^house/(?P<house_id>[0-9]+)/housemate/(?P<housemate_id>[0-9]+)/editInfo$', views.editInfo, name='editInfo'),
+	url(r'^house/(?P<house_id>[0-9]+)/housemate/(?P<housemate_id>[0-9]+)/editHousemate$', views.editHousemate, name='editHousemate'),
 ]
